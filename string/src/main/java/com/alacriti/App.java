f784caf7b9b373af36368b0d7bd12cfbbe8c91f9
mounikad <mounika.dacharla@alacriti.com>
2017-07-13 12:50:24 +0530

    refs #OPAY-1349,new file created

---
+++ string/src/main/java/com/alacriti/App.java
@@ -0,0 +1,20 @@
+package com.alacriti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      StringMani s=new StringMani();
+      s.concat("hello","john");
+      s.equal("ram","Ram");
+      s.startswith("hai ram");
+      s.startswith("hello ram");
+      s.replace("hai");
+      s.length("raheem");
+      
+    }
+}
